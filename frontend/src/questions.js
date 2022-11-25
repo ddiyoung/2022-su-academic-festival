1c32b531e541cb8ad9893d82e079629ed8e42031
hayeon <qorgkdus12@naver.com>
2022-11-26 03:35:37 +0900

    feat: add button click event

---
+++ frontend/src/questions.js
@@ -0,0 +1,53 @@
+export const questions = [
+  {
+    questionText: "먹고 싶은 음식을 골라주세요 [큰 분류]",
+    answerOptions: [
+      { answerText: "단백질이 풍부한 고기, 생선요리" },
+      { answerText: "건어류, 볶음" },
+      { answerText: "분식류" },
+      { answerText: "양념고기 생선" },
+      { answerText: "분식, 튀김류" },
+      { answerText: "해조류 튀김" },
+      { answerText: "탕, 전골, 찌개" },
+      { answerText: "장어구이" },
+      { answerText: "찌개" },
+    ],
+  },
+  {
+    questionText: "먹고 싶은 음식을 골라주세요 [작은 분류]",
+    answerOptions: [
+      { answerText: "튀김, 패스트푸드", id: 1 },
+      { answerText: "튀김류, 까르보나라" },
+      { answerText: "육류튀김" },
+      { answerText: "조림" },
+      { answerText: "분식류" },
+      { answerText: "튀김류 " },
+      { answerText: "피자, 샌드위치" },
+    ],
+  },
+  {
+    questionText: "어떤 종류의 음식을 먹고 싶으신가요?",
+    answerOptions: [
+      { answerText: "한식" },
+      { answerText: "일식" },
+      { answerText: "중식" },
+      { answerText: "양식" },
+      { answerText: "패스트푸드" },
+      { answerText: "분식" },
+    ],
+  },
+  {
+    questionText: "국물 음식을 드시고 싶으신가요?",
+    answerOptions: [
+      { answerText: "O" },
+      { answerText: "X" },
+    ],
+  },
+  {
+    questionText: "매운 음식을 드시고 싶으신가요?",
+    answerOptions: [
+      { answerText: "O" },
+      { answerText: "X" },
+    ],
+  },
+];
