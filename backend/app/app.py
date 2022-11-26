c6ae9cbb296013bae019b026b7c617d3f82adb13
ddiyoung <wsw3998@naver.com>
2022-11-26 20:33:47 +0900

    delete: User

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-from app.routers import User
 from app.routers import Food
 
 
 def create_app():
     app = FastAPI()
 
-    app.include_router(User.UserRouter)
     app.include_router(Food.FoodRouter)
+
     return app
