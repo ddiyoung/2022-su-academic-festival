9d680590bd16eff6304f6b72c2c79ef2ed612512
hayeon <qorgkdus12@naver.com>
2022-11-29 06:12:53 +0900

    fix: change question order and answer

--- frontend/src/questions.js
+++ frontend/src/questions.js
@@ -1,53 +1,88 @@
 export const questions = [
-  {
-    questionText: "먹고 싶은 음식을 골라주세요 [큰 분류]",
-    answerOptions: [
-      { answerText: "단백질이 풍부한 고기, 생선요리" },
-      { answerText: "건어류, 볶음" },
-      { answerText: "분식류" },
-      { answerText: "양념고기 생선" },
-      { answerText: "분식, 튀김류" },
-      { answerText: "해조류 튀김" },
-      { answerText: "탕, 전골, 찌개" },
-      { answerText: "장어구이" },
-      { answerText: "찌개" },
-    ],
-  },
-  {
-    questionText: "먹고 싶은 음식을 골라주세요 [작은 분류]",
-    answerOptions: [
-      { answerText: "튀김, 패스트푸드", id: 1 },
-      { answerText: "튀김류, 까르보나라" },
-      { answerText: "육류튀김" },
-      { answerText: "조림" },
-      { answerText: "분식류" },
-      { answerText: "튀김류 " },
-      { answerText: "피자, 샌드위치" },
-    ],
-  },
-  {
+    {
     questionText: "어떤 종류의 음식을 먹고 싶으신가요?",
     answerOptions: [
-      { answerText: "한식" },
-      { answerText: "일식" },
-      { answerText: "중식" },
-      { answerText: "양식" },
-      { answerText: "패스트푸드" },
-      { answerText: "분식" },
+      { id:0,answerText: "한식" },
+      { id:1,answerText: "일식" },
+      { id:2,answerText: "중식" },
+      { id:3,answerText: "양식" },
+      { id:4,answerText: "패스트푸드" },
+      { id:5,answerText: "분식" },
     ],
   },
   {
-    questionText: "국물 음식을 드시고 싶으신가요?",
+    questionText: "먹고 싶은 음식을 골라주세요",
     answerOptions: [
-      { answerText: "O" },
-      { answerText: "X" },
+      { id:0, answerText: "단백질이 풍부한 고기, 생선요리" },
+      { id:1, answerText: "건어류, 볶음" },
+      { id:2, answerText: "분식류" },
+      { id:3, answerText: "양념고기 생선" },
+      { id:4, answerText: "분식, 튀김류" },
+      { id:5, answerText: "해조류 튀김" },
+      { id:6, answerText: "탕, 전골, 찌개" },
+      { id:7, answerText: "장어구이" },
+      { id:8, answerText: "찌개" },
+    ],
+  },
+  { //small label
+    questionText: "먹고 싶은 음식을 골라주세요",
+    answerOptions: [
+      { id:0, answerText: "멸치볶음"},
+      { id:1, answerText: "삼겹살구이" },
+      { id:2, answerText: "장어구이" },
+      { id:3, answerText: "김치찌개" },
+      { id:4, answerText: "다시마튀김" },
+
+      { id:5, answerText: "덮밥" },//5
+      { id:6, answerText: "찜닭" },
+      { id:7, answerText: "토마토파스타"},
+      { id:8, answerText: "모둠초밥" },
+      { id:9, answerText: "해물볶음밥" },
+
+      { id:10, answerText: "김튀김" },//10
+      { id:11, answerText: "감자조림" },
+      { id:12, answerText: "부대찌개" },
+      { id:13, answerText: "된장국" },
+      { id:14, answerText: "뼈다귀해장국" },
+      
+      { id:15,answerText: "김치말이국수" },//15
+      { id:16,answerText: "물냉면" },
+      { id:17,answerText: "죽" },
+      { id:18,answerText: "돼지고기찌개" },
+      { id:19,answerText: "쇠고기무국" },
+            
+      { id:20,answerText: "우거지된장국" },//20
+      { id:21,answerText: "고구마튀김" },
+      { id:22, answerText: "치킨까르보나라" },
+      { id:23, answerText: "숯불양념구이치킨" },
+      { id:24, answerText: "우엉조림" },  
+      
+      { id:25, answerText: "볶음라면" },//25     
+      { id:26, answerText: "게살튀김" },
+      { id:27, answerText: "치킨바베큐 피자" },
+      { id:28, answerText: "꽁치김치찌개" },
+      { id:29, answerText: "돼지등심돈가스" },
+      
+      { id:30, answerText: "양념게장" },//30            
+      { id:31, answerText: "갈치구이" },
+      { id:32, answerText: "햄엔치즈샌드위치" },
+      { id:33, answerText: "오리불고기" },
+      { id:34, answerText: "해물크림소스스파게티" },//34
     ],
   },
   {
     questionText: "매운 음식을 드시고 싶으신가요?",
     answerOptions: [
-      { answerText: "O" },
-      { answerText: "X" },
+      { id: true, answerText: "O" },
+      { id: false, answerText: "X" },
     ],
   },
+  {
+    questionText: "국물 음식을 드시고 싶으신가요?",
+    answerOptions: [
+      { id: true, answerText: "O" },
+      { id: false, answerText: "X" },
+    ],
+  },
+
 ];
