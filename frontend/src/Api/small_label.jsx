a334fa1505d22aef49495e15a59d64fe28f105c0
hayeon <qorgkdus12@naver.com>
2022-11-30 02:09:12 +0900

    style: remove debuggin code

--- frontend/src/Api/small_label.jsx
+++ frontend/src/Api/small_label.jsx
@@ -3,9 +3,7 @@ import { SERVER, SMALL_LABEL_PATH, SMALL_LABEL_PARAMS } from "../constants";
 // third question
 export const api_small_label = async (Q1, Q2, data) => {
   var list;
-  var url = SERVER + SMALL_LABEL_PATH + Q1 + "&" + Q2 + "&" + SMALL_LABEL_PARAMS + data;
-  //await console.log(data);
-  //await console.log(url);
+
   await fetch(
     SERVER + SMALL_LABEL_PATH + Q1 + "&" + Q2 + "&" + SMALL_LABEL_PARAMS + data,
     {
