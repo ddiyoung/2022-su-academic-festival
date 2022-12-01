fdc03bf6c67e90227d63f8c9e234e05be86bbb66
ddiyoung <wsw3998@naver.com>
2022-12-02 01:00:59 +0900

    fix: keywordrank function to okt

--- backend/app/utils/keyword.py
+++ backend/app/utils/keyword.py
@@ -1,13 +1,6 @@
-from krwordrank.word import KRWordRank
-
-min_count = 1
-max_length = 10
-
-beta = 0.85
-max_iter = 10
+from konlpy.tag import Okt
 
 
 def keywordrank(keyword: str):
-    wordrank_extractor = KRWordRank(min_count=min_count, max_length=max_length)
-    keywords, rank, graph = wordrank_extractor.extract([keyword], beta, max_iter)
-    return keywords.keys()
+    okt = Okt()
+    return okt.nouns(keyword)
