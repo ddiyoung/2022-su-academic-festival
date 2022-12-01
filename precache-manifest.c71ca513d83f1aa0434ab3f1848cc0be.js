ffa35b84d09bfb01d4e025767deb83c07dab5e22
hayeon <qorgkdus12@naver.com>
2022-12-01 18:43:30 +0900

    Updates

---
+++ precache-manifest.c71ca513d83f1aa0434ab3f1848cc0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "340aab7207a58b9ca294",
+    "url": "/2022-su-academic-festival/static/css/main.01eedc3e.chunk.css"
+  },
+  {
+    "revision": "340aab7207a58b9ca294",
+    "url": "/2022-su-academic-festival/static/js/main.340aab72.chunk.js"
+  },
+  {
+    "revision": "ae4700ead3b6e1632dda",
+    "url": "/2022-su-academic-festival/static/js/1.ae4700ea.chunk.js"
+  },
+  {
+    "revision": "5b072c460ee04fb143a7",
+    "url": "/2022-su-academic-festival/static/js/2.5b072c46.chunk.js"
+  },
+  {
+    "revision": "d0c2926864eda9f24e6a",
+    "url": "/2022-su-academic-festival/static/js/runtime~main.d0c29268.js"
+  },
+  {
+    "revision": "ae9c6882c6009503035799615fdc44bf",
+    "url": "/2022-su-academic-festival/index.html"
+  }
+];
