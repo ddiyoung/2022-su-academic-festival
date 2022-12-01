b686de2396cb3c74c124fc0bb0d38065fc57f84f
ddiyoung <ddiyoung@sju.ac.kr>
2022-12-02 00:23:36 +0900

    fix: delete build

---
+++ frontend/build/precache-manifest.c19a88770374a88e51bd58c721fda5fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "408aa7ea29c8bfec66e5",
+    "url": "/static/css/main.462b9c6c.chunk.css"
+  },
+  {
+    "revision": "408aa7ea29c8bfec66e5",
+    "url": "/static/js/main.408aa7ea.chunk.js"
+  },
+  {
+    "revision": "ae4700ead3b6e1632dda",
+    "url": "/static/js/1.ae4700ea.chunk.js"
+  },
+  {
+    "revision": "ec7ed14a0d838d667faa",
+    "url": "/static/js/2.ec7ed14a.chunk.js"
+  },
+  {
+    "revision": "95228a3a9e71a7d2dd9f",
+    "url": "/static/js/runtime~main.95228a3a.js"
+  },
+  {
+    "revision": "6a280bfa32b431527048e1818624bba5",
+    "url": "/index.html"
+  }
+];
