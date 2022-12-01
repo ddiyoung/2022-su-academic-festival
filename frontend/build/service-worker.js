9c50adf8e3ed986b710fd0c7889d01c633ea4630
hayeon <qorgkdus12@naver.com>
2022-12-01 14:19:19 +0900

    fix: build path error

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/2022-su-academic-festival/precache-manifest.805a3b59b9ca3615a6826a118885c498.js"
+  "./precache-manifest.bad2c62277c53eda1c6cbd5592ae0944.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/2022-su-academic-festival/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
